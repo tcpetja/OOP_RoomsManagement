b31e185f92f8ba065257cad7e78d8e6731484341
Clement Petja <tcpetja@gmail.com>
2025-01-15 00:31:39 +0200

    Add files via upload

---
+++ RoomManagementApp/src/ResortEntities/Room.java
@@ -0,0 +1,14 @@
+
+package ResortEntities;
+
+import java.util.Date;
+
+
+public class Room {
+    Integer roomNo;
+    String suiteType;
+    String roomStatus;
+    Date checkInDate;
+    Date checkOutDate;
+    
+}
